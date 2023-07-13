206e641ae5b1009504254ffa1f7a53bbbd332ab1
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-13 17:04:45 -0500

    Se terminó de poder editar los gastos - Clase 120

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import ListadoGastos from './components/ListadoGastos'
 import Modal from './components/Modal'
@@ -9,13 +9,28 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg'
 function App() {
 
   const [gastos, setGastos] = useState([]);
+
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
   const [modal, setModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false);
 
+  const [gastoEditar, setGastoEditar] = useState({});
+
+  useEffect(() => {
+    if(Object.keys(gastoEditar).length > 0){
+      setModal(true)
+
+      setTimeout(() => {
+        setAnimarModal(true)
+      }, 500);
+    }
+  }, [gastoEditar])
+
   const handleNuevoGasto = () => {
     setModal(true)
+    setGastoEditar({})
 
     setTimeout(() => {
       setAnimarModal(true)
@@ -23,10 +38,16 @@ function App() {
   }
 
   const guardarGasto = gasto => {
-    gasto.id = generarId();
-    gasto.fecha = Date.now();
-    setGastos([...gastos, gasto])
-
+    if(gasto.id){
+      // Actualizar
+      const gastosActualizados = gastos.map( gastoState => gastoState.id === gasto.id ? gasto : gastoState)
+      setGastos(gastosActualizados)
+    }else{
+      // Nuevo gasto
+      gasto.id = generarId();
+      gasto.fecha = Date.now();
+      setGastos([...gastos, gasto])
+    }
     setAnimarModal(false)
     setTimeout(() => {
         setModal(false)
@@ -48,6 +69,7 @@ function App() {
           <main>
             <ListadoGastos
               gastos={gastos}
+              setGastoEditar={setGastoEditar}
             />
           </main>
           <div className='nuevo-gasto'>
@@ -61,6 +83,7 @@ function App() {
         animarModal={animarModal}
         setAnimarModal={setAnimarModal}
         guardarGasto={guardarGasto}
+        gastoEditar={gastoEditar}
       />}
     </div>
   )
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Mensaje from './Mensaje'
 import CerrarBtn from '../img/cerrar.svg'
 
-const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
+const Modal = ({
+    setModal,
+    animarModal,
+    setAnimarModal,
+    guardarGasto,
+    gastoEditar
+}) => {
 
     const [mensaje, setMensaje ] = useState('');
     const [nombre, setNombre] = useState('');
     const [cantidad, setCantidad] = useState('');
     const [categoria, setCategoria] = useState('');
+    const [fecha, setFecha] = useState('');
+    const [id, setId] = useState('');
+
+    useEffect(() => {
+        if(Object.keys(gastoEditar).length > 0){
+            setNombre(gastoEditar.nombre)
+            setCantidad(gastoEditar.cantidad)
+            setCategoria(gastoEditar.categoria)
+            setId(gastoEditar.id)
+            setFecha(gastoEditar.fecha)
+            }
+    }, []);
 
     const ocultarModal = () => {
         setAnimarModal(false)
@@ -28,7 +46,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
             return;
         }
 
-        guardarGasto({nombre, cantidad, categoria})
+        guardarGasto({nombre, cantidad, categoria, id, fecha})
     }
 
   return (
@@ -38,7 +56,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
         </div>
 
         <form onSubmit={handleSubmit} className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
-            <legend>Nuevo Gasto</legend>
+            <legend>{gastoEditar.nombre ? 'Editar gasto' : 'Nuevo gasto'}</legend>
             {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
 
             <div className='campo'>
@@ -63,7 +81,7 @@ const Modal = ({setModal, animarModal, setAnimarModal, guardarGasto}) => {
                 </select>
             </div>
 
-            <input type="submit" value='Añadir gasto'/>
+            <input type="submit" value={gastoEditar.nombre ? 'Guardar cambios' : 'Añadir gasto'}/>
         </form>
     </div>
   )
